13be80a15e6679e4d147921e6cc57cd7ee05f4a1
Richard Opiyo <romenyo@dewcis.com>
2022-05-13 01:42:44 +0300

    add  basic ui to the schol library project

--- rental.rb
+++ rental.rb
@@ -5,9 +5,9 @@ class Rental
     @date = date
 
     @person = person
-    person.rentals << self
+    # person.rentals << self
 
     @book = book
-    book.rentals << self
+    # book.rentals << self
   end
 end
